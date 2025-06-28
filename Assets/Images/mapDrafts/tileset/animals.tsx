f70da3e234ab1df23060c288a08128bafb4741b8
Lucas Chaves <gabrielucas2008@hotmail.com>
2025-06-28 17:37:46 -0300

    changes screen width and height; updates gitignore; adds tsx files.

---
+++ Assets/Images/mapDrafts/tileset/animals.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="animals" tilewidth="16" tileheight="16" tilecount="1080" columns="108">
+ <image source="../assets/Cute_Fantasy/Animals/animals.png" trans="000000" width="1728" height="160"/>
+</tileset>
---
+++ Assets/Images/mapDrafts/tileset/golem_laser.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="golem_laser" tilewidth="16" tileheight="16" tilecount="1674" columns="18">
+ <image source="../assets/Kronovi/Mecha-stone Golem 0.1/weapon PNG/Laser_sheet.png" trans="000000" width="300" height="1500"/>
+</tileset>
