e567f87d3516cb3ede281bffa38f1fe64eba6337
Lucas Chaves <gabrielucas2008@hotmail.com>
2025-06-29 01:26:46 -0300

    feat: updates Tiled's maps.

--- Assets/Images/mapDrafts/tileset/chars.tsx
+++ Assets/Images/mapDrafts/tileset/chars.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="chars" tilewidth="16" tileheight="16" tilecount="156" columns="13">
- <image source="chars.png" trans="000000" width="208" height="192"/>
+ <image source="../assets/Cute_Fantasy/Custom/chars.png" width="208" height="192"/>
 </tileset>
